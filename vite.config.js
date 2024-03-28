1fb504a7d9ee20a6dbca6f075983442b5555e1f8
Shahzaib <shaulazain@gmail.com>
2024-03-28 16:20:56 +0500

    secoind comit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Bloging_Web_React/"
+  base:"/Bloging_Web_React/",
   plugins: [react()],
 })
